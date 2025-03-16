462fb3825d1490960ba9c5140ae5297d5dcab45f
manimekala123 <139643976+manimekala123@users.noreply.github.com>
2025-03-16 18:40:39 +0530

    do third question

--- three.java
+++ three.java
@@ -1,7 +1,7 @@
-public class two{
+public class three{
     public static void main(String args[]){
-        // 2)print numbers from 1 to 10
-      for (int i = 1; i<=10; i++)
+        // 3)print the numbers in reverse
+      for (int i = 10; i>0; i--)
       {
         System.out.println(i);
       }
