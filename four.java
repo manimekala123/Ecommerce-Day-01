e91ed51a0b891288afd60e89b1fb37b604c79f93
manimekala123 <139643976+manimekala123@users.noreply.github.com>
2025-03-16 18:43:22 +0530

    do fourth question

---
+++ four.java
@@ -0,0 +1,13 @@
+public class four{
+    public static void main(String[] args)
+    {
+        // 4)print the odd numbers between 1 to 20
+      for (int i = 1; i <= 20; i++)
+      {
+        if(i % 2 == 1)
+        {
+           System.out.println(i); 
+        }
+      }
+    }
+}
